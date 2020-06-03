308fc461a4a7938150adbf20b86e622574ef319b
Jeff Kee <jeffkhm@gmail.com>
2020-06-02 17:46:25 -0700

    calculate average lenght reviews

--- r_amazon_analytics.py
+++ r_amazon_analytics.py
@@ -6,8 +6,11 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 10000 == 0:  # % 是用来求余数
 		   print(len(data))
-print(len(data)) 
+print('档案读取完了, 总共有', len(data), '笔资料') 
 
-print(data[0])
-print('---------------------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)  # sum_len += len(d)
+    
+
+print('留言的平均长度为', sum_len/len(data))
