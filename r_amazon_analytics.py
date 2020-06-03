878579a6033376bd41fd8415f31e9361a71e4ed3
Jeff Kee <jeffkhm@gmail.com>
2020-06-02 20:03:39 -0700

    add filtering

--- r_amazon_analytics.py
+++ r_amazon_analytics.py
@@ -1,4 +1,4 @@
-data = []
+data = []    # question 1
 count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
@@ -14,3 +14,13 @@ for d in data:
     
 
 print('留言的平均长度为', sum_len/len(data))
+
+
+new = []   # question 2
+for d in data:# for d in data 是这个data清单一笔一笔数据叫出来 # 每一个"d" 是单独留言
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '留笔言张度小於100') 
+print(new[0])
+print(new[2]) 
+
